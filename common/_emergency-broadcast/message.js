df8e184e4398d8fcded6ebc2f4d0bd225a7a064f
Jinhao <jinhaoliu18@hotmail.com>
2020-06-11 19:24:53 -0700

    changed custom domain

--- common/_emergency-broadcast/message.js
+++ common/_emergency-broadcast/message.js
@@ -27,7 +27,7 @@ if(document.URL.indexOf("tritON/profile/SAML2/Redirect/SSO") >= 0){
     (function($) {
         $(document).ready(function() {
     		$.ajax( {
-    			url:"https://jinhaoliudemo.azurewebsites.net/common/_emergency-broadcast/js-message.jsonp?callback=?",
+    			url:"https://cse190grunt.azurewebsites.net/common/_emergency-broadcast/js-message.jsonp?callback=?",
     			dataType: "jsonp",
     			jsonpCallback:"showEmergencyMessage"
     		});
